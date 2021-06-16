c5797c518b06ec7a5d714af2c5c428b554eb0eef
Moumen Hallak <moumen95@gmail.com>
2021-06-16 21:20:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+const titleCased = () =>tutorials.map(tutorial => tutorial.split(" ").map(word => word[0])) {
   return tutorials
 }
